5b4afb5257490695a162728d629215a30bcf8414
Marc Dominik Migge <marcmigge@gmx.net>
2021-12-06 21:22:10 +0100

    Finish day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,19 +1,51 @@
-use std::io::BufRead;
+use std::{collections::HashMap, io::BufRead};
+
+fn total_descendants(days_remaining: i32, memo: &mut HashMap<i32, u64>) -> u64 {
+    if memo.contains_key(&days_remaining) {
+        return memo[&days_remaining];
+    }
+
+    let mut descendants = 1;
+
+    if days_remaining <= 0 {
+        memo.insert(days_remaining, descendants);
+        return descendants;
+    }
+
+    let mut tmp_days_remaining = days_remaining;
+    descendants += 1;
+    while tmp_days_remaining > 7 {
+        descendants += total_descendants(tmp_days_remaining - 9, memo);
+        tmp_days_remaining -= 7;
+    }
+
+    memo.insert(days_remaining, descendants);
+    return descendants;
+}
 
 fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let file = std::fs::File::open(filename).unwrap();
-    let line = std::io::BufReader::new(file).lines().next().unwrap().unwrap();
+    let line = std::io::BufReader::new(file)
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
 
-    let mut fish_popoluation = line.split(',')
-        .map(|timer| timer.parse::<u32>().unwrap())
+    let original_fish_popoluation = line
+        .split(',')
+        .map(|timer| timer.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
+    let mut fish_popoluation = original_fish_popoluation.clone();
     for _ in 0..80 {
         let new_fish = fish_popoluation.iter().filter(|timer| **timer == 0).count();
 
-        fish_popoluation = fish_popoluation.iter().map(|timer | if *timer == 0 { 6 } else { timer - 1 }).collect();
-        
+        fish_popoluation = fish_popoluation
+            .iter()
+            .map(|timer| if *timer == 0 { 6 } else { timer - 1 })
+            .collect();
+
         for _ in 0..new_fish {
             fish_popoluation.push(8);
         }
@@ -21,4 +53,11 @@ fn main() {
 
     let part1 = fish_popoluation.len();
     println!("{}", part1);
+
+    let mut memo = HashMap::new();
+    let part2 = original_fish_popoluation
+        .iter()
+        .map(|timer| total_descendants(256 - timer, &mut memo))
+        .sum::<u64>();
+    println!("{}", part2);
 }
