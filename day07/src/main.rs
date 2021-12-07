305ba3a0c9fc4fb9c3b4bd8bdd622a4014028212
Marc Dominik Migge <marcmigge@gmx.net>
2021-12-07 14:00:22 +0100

    Finish day 7

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -25,11 +25,42 @@ fn main() {
             .sum::<usize>();
 
         min_fuel = match min_fuel {
-            Some(old_fuel) => if fuel < old_fuel { Some(fuel) } else { Some(old_fuel) },
+            Some(old_fuel) => {
+                if fuel < old_fuel {
+                    Some(fuel)
+                } else {
+                    Some(old_fuel)
+                }
+            }
             None => Some(fuel),
         };
     }
 
     let part1 = min_fuel.unwrap();
     println!("{}", part1);
+
+    let mut min_fuel = None;
+    for destination in min_position..=max_position {
+        let fuel = crab_positions
+            .iter()
+            .map(|position| {
+                let n = (destination as i32 - *position as i32).abs();
+                (n * (n + 1) / 2) as usize
+            })
+            .sum::<usize>();
+
+        min_fuel = match min_fuel {
+            Some(old_fuel) => {
+                if fuel < old_fuel {
+                    Some(fuel)
+                } else {
+                    Some(old_fuel)
+                }
+            }
+            None => Some(fuel),
+        };
+    }
+
+    let part2 = min_fuel.unwrap();
+    println!("{}", part2);
 }
