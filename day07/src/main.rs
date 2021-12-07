73c400a944410f35ba6dc4970d68001f5e615a6f
Marc Dominik Migge <marcmigge@gmx.net>
2021-12-07 13:56:59 +0100

    Finish day 7, part 1

---
+++ day07/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::BufRead;
+
+fn main() {
+    let filename = std::env::args().nth(1).unwrap();
+    let file = std::fs::File::open(filename).unwrap();
+    let line = std::io::BufReader::new(file)
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+
+    let crab_positions = line
+        .split(',')
+        .map(|position| position.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    let min_position = *crab_positions.iter().min().unwrap();
+    let max_position = *crab_positions.iter().max().unwrap();
+
+    let mut min_fuel = None;
+    for destination in min_position..=max_position {
+        let fuel = crab_positions
+            .iter()
+            .map(|position| (destination as i32 - *position as i32).abs() as usize)
+            .sum::<usize>();
+
+        min_fuel = match min_fuel {
+            Some(old_fuel) => if fuel < old_fuel { Some(fuel) } else { Some(old_fuel) },
+            None => Some(fuel),
+        };
+    }
+
+    let part1 = min_fuel.unwrap();
+    println!("{}", part1);
+}
