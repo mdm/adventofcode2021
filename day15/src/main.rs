7c393c6a08f69eb6519d8a7d3f91dadd7df6a85f
Marc Dominik Migge <marcmigge@gmx.net>
2021-12-15 19:33:15 +0100

    Improve performance for day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,12 +1,10 @@
-use std::collections::HashMap;
 use std::io::BufRead;
 
 use priority_queue::PriorityQueue;
 
-
-fn best_path_cost(risk_levels: &HashMap<(i32, i32), i32>) -> i32 {
-    let max_x = *risk_levels.keys().map(|(x, _y)| x).max().unwrap();
-    let max_y = *risk_levels.keys().map(|(_x, y)| y).max().unwrap();
+fn best_path_cost(risk_levels: &Vec<Vec<i32>>) -> i32 {
+    let max_x = risk_levels[0].len() - 1;
+    let max_y = risk_levels.len() - 1;
 
     let mut queue = PriorityQueue::new();
     queue.push((0, 0), -0);
@@ -16,13 +14,20 @@ fn best_path_cost(risk_levels: &HashMap<(i32, i32), i32>) -> i32 {
             return -risk_so_far;
         }
 
-        let offsets = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
-        for neighbor in offsets.iter().map(|offset| (current.0 + offset.0, current.1 + offset.1)) {
-            if !risk_levels.contains_key(&neighbor) {
-                continue; // off grid
-            }
+        if current.1 > 0 {
+            queue.push_increase((current.0, current.1 - 1), risk_so_far - risk_levels[current.1 - 1][current.0]);
+        }
 
-            queue.push_increase(neighbor, risk_so_far - risk_levels[&neighbor]);
+        if current.0 < max_x {
+            queue.push_increase((current.0 + 1, current.1), risk_so_far - risk_levels[current.1][current.0 + 1]);
+        }
+
+        if current.1 < max_y {
+            queue.push_increase((current.0, current.1 + 1), risk_so_far - risk_levels[current.1 + 1][current.0]);
+        }
+
+        if current.0 > 0 {
+            queue.push_increase((current.0 - 1, current.1), risk_so_far - risk_levels[current.1][current.0 - 1]);
         }
     }
 
@@ -34,28 +39,34 @@ fn main() {
     let file = std::fs::File::open(filename).unwrap();
     let reader = std::io::BufReader::new(file);
 
-    let mut risk_levels = HashMap::new();
-    for (y, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        for (x, char) in line.chars().enumerate() {
-            let risk_level = char.to_digit(10).unwrap() as i32;
-            risk_levels.insert((x as i32, y as i32), risk_level);
-        }
-    }
+    let risk_levels = reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            line.chars()
+                .map(|char| char.to_digit(10).unwrap() as i32)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
     let part1 = best_path_cost(&risk_levels);
     println!("{}", part1);
 
-    let offset_x = *risk_levels.keys().map(|(x, _y)| x).max().unwrap() + 1;
-    let offset_y = *risk_levels.keys().map(|(_x, y)| y).max().unwrap() + 1;
+    let offset_x = risk_levels[0].len();
+    let offset_y = risk_levels.len();
 
-    let mut risk_levels2 = HashMap::new();
-    for y in 0..5 {        
-        for x in 0..5 {
-            for (position, risk_level) in &risk_levels {
-                risk_levels2.insert((x * offset_x + position.0, y * offset_y + position.1), (*risk_level - 1 + x + y) % 9 + 1);
+    let mut risk_levels2 = Vec::new();
+    for tile_y in 0..5 {
+        for y in 0..offset_y {
+            let mut row = Vec::new();
+            for tile_x in 0..5 {
+                for x in 0..offset_x {
+                    row.push((risk_levels[y][x] - 1 + tile_x + tile_y) % 9 + 1)
+                }
             }
-        }   
+
+            risk_levels2.push(row);
+        }
     }
 
     let part2 = best_path_cost(&risk_levels2);
