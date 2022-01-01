3d8594dd7b48e410b3037afa5a28d65e4d318381
Marc Dominik Migge <marcmigge@gmx.net>
2022-01-01 02:34:51 +0100

    Finish day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -177,8 +177,6 @@ fn main() {
         }
     }
 
-    dbg!(on_cubes.len());
-
     let part1 = on_cubes
         .iter()
         .filter(|(x, y, z)| *x >= -50 && *x <= 50 && *y >= -50 && *y <= 50 && *z >= -50 && *z <= 50)
@@ -187,78 +185,27 @@ fn main() {
 
     let mut current_on_cuboids: Vec<Cuboid> = Vec::new();
     let mut next_on_cuboids: Vec<Cuboid> = Vec::new();
-    let mut part2 = 0i64;
     for (on, new) in reboot_steps {
-        let mut intersections = 0;
-
         for old in current_on_cuboids {
             let intersect_x = new.max_x >= old.min_x && old.max_x >= new.min_x;
             let intersect_y = new.max_y >= old.min_y && old.max_y >= new.min_y;
             let intersect_z = new.max_z >= old.min_z && old.max_z >= new.min_z;
 
             if intersect_x && intersect_y && intersect_z {
-                intersections += 1;
-
-                // let old_contains_new_x = new.min_x >= old.min_x && new.max_x <= old.max_x;
-                // let old_contains_new_y = new.min_y >= old.min_y && new.max_y <= old.max_y;
-                // let old_contains_new_z = new.min_z >= old.min_z && new.max_z <= old.max_z;
-                // let old_contains_new = old_contains_new_x && old_contains_new_y && old_contains_new_z;
-
-                // let new_contains_old_x = old.min_x >= new.min_x && old.max_x <= new.max_x;
-                // let new_contains_old_y = old.min_y >= new.min_y && old.max_y <= new.max_y;
-                // let new_contains_old_z = old.min_z >= new.min_z && old.max_z <= new.max_z;
-                // let new_contains_old = new_contains_old_x && new_contains_old_y && new_contains_old_z;
-
-                // dbg!(old_contains_new, new_contains_old);
-                // println!();
-
-                let intersection = Cuboid {
-                    min_x: old.min_x.max(new.min_x),
-                    max_x: old.max_x.min(new.max_x),
-                    min_y: old.min_y.max(new.min_y),
-                    max_y: old.max_y.min(new.max_y),
-                    min_z: old.min_z.max(new.min_z),
-                    max_z: old.max_z.min(new.max_z),
-                };
-
-                part2 -= intersection.volume();
-
-                // dbg!(on);
-
-                if on {
-                    part2 += new.volume();
-                    // dbg!(&new, &old);
-                    let new_on = split(&new, &old);
-                    // dbg!(new_on.len(), &new_on);
-                    next_on_cuboids.extend(new_on);
-                    next_on_cuboids.push(old);
-                } else {
-                    // dbg!(&old, &new);
-                    let new_off = split(&old, &new);
-                    // dbg!(&new_off);
-                    next_on_cuboids.extend(new_off);
-                }
-
-                dbg!(part2);
+                let parts = split(&old, &new);
+                next_on_cuboids.extend(parts);
             } else {
                 next_on_cuboids.push(old);
             }
         }
 
-        if on && intersections == 0 {
-            part2 += new.volume();
+        if on {
             next_on_cuboids.push(new);
         }
-
-        // dbg!(&next_on_cuboids);
-        
+      
         current_on_cuboids = next_on_cuboids;
         next_on_cuboids = Vec::new();
-
-        dbg!(current_on_cuboids.len());
     }
-
-    dbg!(part2);
 
     let part2 = current_on_cuboids
         .iter()
