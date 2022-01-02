b1aa08c09b74cd5c478a8d4438f12857ee92cc4b
Marc Dominik Migge <marcmigge@gmx.net>
2022-01-02 11:38:47 +0100

    Day 24 WIP

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::BufRead, collections::HashSet};
+use std::{io::BufRead};
 
 #[derive(Debug, Clone)]
 enum Operand {
@@ -42,6 +42,85 @@ impl Instruction {
     }
 }
 
+fn execute(program: &Vec<Instruction>, input: i64, mut registers: Vec<i64>, mut ip: usize) -> (Vec<i64>, usize) {
+    loop {
+        match &program[ip] {
+            Instruction::Inp(dest) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                registers[dest] = input;
+            }
+            Instruction::Add(dest, src) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let src = match src {
+                    Operand::Register(src) => registers[*src],
+                    Operand::Constant(value) => *value,
+                };
+                registers[dest] = registers[dest] + src;
+            }
+            Instruction::Mul(dest, src) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let src = match src {
+                    Operand::Register(src) => registers[*src],
+                    Operand::Constant(value) => *value,
+                };
+                dbg!(registers[dest], src);
+                registers[dest] = registers[dest] * src;
+            }
+            Instruction::Div(dest, src) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let src = match src {
+                    Operand::Register(src) => registers[*src],
+                    Operand::Constant(value) => *value,
+                };
+                registers[dest] = registers[dest] / src;
+            }
+            Instruction::Mod(dest, src) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let src = match src {
+                    Operand::Register(src) => registers[*src],
+                    Operand::Constant(value) => *value,
+                };
+                registers[dest] = registers[dest] % src;
+            }
+            Instruction::Eql(dest, src) => {
+                let dest = match dest {
+                    Operand::Register(dest) => *dest,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let src = match src {
+                    Operand::Register(src) => registers[*src],
+                    Operand::Constant(value) => *value,
+                };
+                registers[dest] = if registers[dest] == src { 1 } else { 0 };
+            }
+        }
+
+        ip += 1;
+        dbg!(ip);
+
+        if let Instruction::Inp(_) = program[ip] {
+            break;
+        }
+    }
+
+    (registers, ip)
+}
+
 fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let file = std::fs::File::open(filename).unwrap();
@@ -51,89 +130,20 @@ fn main() {
         .lines()
         .map(|line| Instruction::from_string(&line.unwrap()))
         .collect::<Vec<_>>();
+        
+    let mut outputs1 = Vec::new();
+    let mut outputs2 = Vec::new();
+    for input1 in 1..=9 {
+        let mut start = 0;
+        let (registers, ip) = execute(&program, input1, vec![0i64; 4], start);
+        outputs1.push(registers.clone());
+        start = ip;
 
-    let start = 0;
-    let mut outputs = HashSet::new();
-    for input in 1..=9 {
-        let mut ip = start;
-        let mut registers = vec![0i64; 4];
-
-        loop {
-            match &program[ip] {
-                Instruction::Inp(dest) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    registers[dest] = input;
-                }
-                Instruction::Add(dest, src) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    let src = match src {
-                        Operand::Register(src) => registers[*src],
-                        Operand::Constant(value) => *value,
-                    };
-                    registers[dest] = registers[dest] + src;
-                }
-                Instruction::Mul(dest, src) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    let src = match src {
-                        Operand::Register(src) => registers[*src],
-                        Operand::Constant(value) => *value,
-                    };
-                    dbg!(registers[dest], src);
-                    registers[dest] = registers[dest] * src;
-                }
-                Instruction::Div(dest, src) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    let src = match src {
-                        Operand::Register(src) => registers[*src],
-                        Operand::Constant(value) => *value,
-                    };
-                    registers[dest] = registers[dest] / src;
-                }
-                Instruction::Mod(dest, src) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    let src = match src {
-                        Operand::Register(src) => registers[*src],
-                        Operand::Constant(value) => *value,
-                    };
-                    registers[dest] = registers[dest] % src;
-                }
-                Instruction::Eql(dest, src) => {
-                    let dest = match dest {
-                        Operand::Register(dest) => *dest,
-                        Operand::Constant(_) => unreachable!(),
-                    };
-                    let src = match src {
-                        Operand::Register(src) => registers[*src],
-                        Operand::Constant(value) => *value,
-                    };
-                    registers[dest] = if registers[dest] == src { 1 } else { 0 };
-                }
-            }
-
-            ip += 1;
-            dbg!(ip);
-
-            if let Instruction::Inp(_) = program[ip] {
-                outputs.insert(registers.clone());
-                break;
-            }
-        }
+        for input2 in 1..=9 {
+            let (registers, ip) = execute(&program, input2, outputs1[input1 as usize - 1].clone(), start);
+            outputs2.push(registers.clone());
+        }        
     }
 
-    dbg!(&outputs);
+    dbg!(&outputs1, outputs2);
 }
